761f6b5da9d5affa9608310ee8b32a2bad1ac4b0
Unesio-Severino <unesio.private@gmail.com>
2023-01-12 14:47:32 +0200

    Chore: fix router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import { AppProps } from 'next/app';
 import "../styles/globals.css";
-
 import Navbar from "../Components/Navbar";
 import { AnimatePresence } from "framer-motion";
 import router from 'next/router';
+import { useRouter } from "next/router";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+const router = useRouter();
  return (
     <div className="font-latoBold relative h-screen overflow-hidden py-20 px-12 lg:px-25">
       <Navbar />
